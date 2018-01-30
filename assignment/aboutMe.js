bbc1e7f5d5922455fbd8f42d6ee0ccb04fcb0d1f
Ally <ally@primeacademy.io>
2018-01-30 09:42:52 -0600

    made fixes, added a problem to hard mode

--- assignment/aboutMe.js
+++ assignment/aboutMe.js
@@ -1,38 +1,43 @@
 // BASE MODE:
-// create a variable called `firstName` and assign it the value of your first name
+// 1 - Create a variable called `firstName` and assign it the value of your first name
 
-// create a second variable called `lastName` and assign it the value of your last name
+// 2 - Create a second variable called `lastName` and assign it the value of your last name
 
-// create a third variable called `fullName`, and USING CONCATENATION, assign it the value of your first and last name
+// 3 - Create a third variable called `fullName`, and USING CONCATENATION,
+// assign it the value of your first and last name
 
-// console log the value of `fullName`
+// 4 - Console log the value of `fullName`
 
-// create a variable called `age` and assign it the value of your age
+// 5 - Create a variable called `age` and assign it the value of your age
 
-// console log this sentence: 'My name is ______ ______, and I am ___ years old.'. Refer back to the videos if you need help with this one.
+// 6 - Console log this sentence: 'My name is ______ ______, and I am ___ years old.'.
+// Refer back to the videos if you need help with this one.
 
-// create a variable named `adventurous` and set it to a boolean value (true or false)
+// 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 
-// create a variable named `food`, and set its value to a string of your favorite food
+// 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
-// create a variable called `pets` and set it to the value of the number of pets you have
+// 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 
-// create a variable called `friendsPets` and set it to the value of the number of pets your friend has
+// 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
-// add two pets to your pets variable
+// 11 - Add two pets to your pets variable
 
-// create a constant variable called `allowedPets` and set it to a number value of your choice
+// 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+
+// 13 - Create a conditional: if adventurous is true, console log "Let's go on an adventure!", 
+// if it's not true, console log "How about we stay home?"
 
 
 // HARD MODE:
-// create a conditional: if adventurous is true, console log "Let's go on an adventure!", if it's not true, console log "How about we stay home?"
+// 14 - Create a compound conditional: if age is greater than 18 and adventurous is true, console log "Let's go!"
 
-// create a compound conditional: if age is greater than 18 and adventurous is true, console log "Let's go!"
-
-// Write a conditional that console logs "I can have more pets!" if the value of `pets` is less than the value of `allowedPets`
+// 15 - Write a conditional that console logs "I can have more pets!" if the value of `pets` is less than the value of `allowedPets`,
+// console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
+// and console logs "Oh no, I have too many pets!" if the value of `pets` is greater than the value of `allowedPets`.
 
 // PRO MODE
-// Write a conditional that assigns the value of the addition
+// 16 - Write a conditional that assigns the value of the addition
 // of the `pets` & `friendsPets` variables to a new variable called `totalPets` IF `pets` is greater
 // than `friendsPets`. If `friendsPets` is greater than `pets`, assign the difference of the `pets` & `friendsPets`
 // variables to `totalPets
