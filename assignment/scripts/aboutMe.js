3af66ae98c16d513fe264667d6a0bb93ae5ea91f
Ally <ally@primeacademy.io>
2018-05-03 10:38:46 -0500

    change file structure, change modes, clarify questions

---
+++ assignment/scripts/aboutMe.js
@@ -0,0 +1,46 @@
+// REQUIRED FEATURES:
+// 1 - Create a variable called `firstName` and assign it the value of your first name
+
+// 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+// 3 - Create a third variable called `fullName`, assign it the value of your first and last name
+// (remember, you already have variables for this, can you use those?)
+
+// 4 - Console log the value of `fullName`
+
+// 5 - Create a variable called `age` and assign it the value of your age
+
+// 6 - Console log this sentence, adding in the variables you created above: 
+// 'My name is (full name), and I am (age) years old.'.
+// Refer back to the videos if you need help with this one.
+
+// 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+
+// 8 - Create a variable named `food`, and set its value to a string of your favorite food
+
+// 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+
+// 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+
+// 11 - Add two pets to your `pets` variable
+
+// 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// if it's not true, console log "How about we stay home?"
+
+
+// STRETCH GOALS:
+// 14 - Create a compound conditional: if age is greater than 18 and adventurous is true,
+// console log "Hurray!"
+
+// 15 - Write a conditional that console logs "I can have more pets!" 
+//if the value of `pets` is less than the value of `allowedPets`,
+// console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
+// and console logs "Oh no, I have too many pets!"
+// if the value of `pets` is greater than the value of `allowedPets`.
+
+// 16 - Write a conditional that assigns the value of the `pets` variable 
+// to a new variable called `mostPets` IF `pets` is greater than `friendsPets`.
+// If `friendsPets` is greater than `pets`, assign the value of the `friendsPets`
+// variable to `mostPets`. Console log the value of `mostPets`.
