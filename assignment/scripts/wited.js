5a2f8e50c966cfd9c73b83b8648b8d6d0516338c
Dane Smith <doctorhowser@gmail.com>
2020-10-29 09:50:15 -0500

    Create wited.js

---
+++ assignment/scripts/wited.js
@@ -0,0 +1,25 @@
+// WHAT IS THIS EVEN DOING?
+
+// Each code block below is syntactically correct. Your job is to describe what is happening. 
+// They are in isolation -- variables in one question do not affect other questions.
+
+
+// 0. WRITE YOUR DESCRIPTION HERE
+// //EXAMPLE. We make a variable called number and set it to 1 as a number.
+// Then we increment the number variable. Number is now 2.
+// We check if number is greater than or equal to 2. 2 is equal to 2, so
+// we console.log 'yes'
+
+//CODE
+/*
+let number = 1;
+
+number++;
+
+if (number >= 2) {
+  console.log('yes');
+}
+
+
+
+*/
