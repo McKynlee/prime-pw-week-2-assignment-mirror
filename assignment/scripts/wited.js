2de6c456376588bfea4d28f435354c4b03bb9e56
Dane Smith <doctorhowser@gmail.com>
2020-10-29 11:00:02 -0500

    Update wited.js

--- assignment/scripts/wited.js
+++ assignment/scripts/wited.js
@@ -20,6 +20,115 @@ if (number >= 2) {
   console.log('yes');
 }
 
+*/
+
+
+// 1. WRITE YOUR DESCRIPTION HERE
+// 
+// 
+// 
+//
+
+//CODE
+/*
+let name = 'Dane';
+
+if (name === 'Mary') {
+  console.log('Hi, Mary!');
+} else {
+  console.log('How do you do?');
+}
 
 
 */
+
+// 2. WRITE YOUR DESCRIPTION HERE
+// 
+// 
+// 
+//
+
+//CODE
+/*
+let secret;
+
+let code = 123;
+
+if(code === 123) {
+  secret = 'super';
+  code = code * 2;
+}
+
+if (code > 250) {
+  secret = 'duper'
+}
+
+conosle.log(secret)
+
+*/
+
+// 3. WRITE YOUR DESCRIPTION HERE
+// 
+// 
+// 
+//
+
+//CODE
+/*
+let isStudent = true;
+let age = 34;
+let zip = 55407;
+
+if (isStudent === true && zip > 80000 ) {
+  console.log(`You're a student on the West Coast!`);
+} else if (isStudent === false || age < 30) {
+  console.log('What are your hobbies?');
+} else if (isStudent === true) {
+  console.log('Welcome to Prime!');
+} else {
+  console.log('How about the weather?')
+}
+
+*/
+
+
+
+
+// REVERSE
+// Now, we'll give you a description and some code. Make the code match the description. 
+// Look closely -- things are missing and wrong in the code!
+
+//4. We start with three variables -- colorOne is set to 'blue' and colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//-- it is, so we set colorOne and colorTwo to 'purple'
+
+/*
+let colorOne = 'red';
+let colorTwo = 'blue';
+let mix = true;
+
+if (mix === true) {
+  colorOne = 'purple'
+}
+*/
+
+//5. We start with two variables -- temp is set to 40 and time is set to 4.
+// We check if temp is higher than 39 and if time is greater or equal to  4 
+// -- they are so we console.log 'throw away the food!'
+
+/*
+let temp = 40;
+const time = 4;
+
+if (temp > 39 || time >= 4) {
+  console.log('throw away the food!)
+}
+*/
+
+//6. We start with two variables -- temp is set to 40 and time is set to 4.
+// We check if temp is higher than 39 and if time is greater or equal to  4 
+// -- they are so we console.log 'throw away the food!'
+
+/*
+
+*/
+
