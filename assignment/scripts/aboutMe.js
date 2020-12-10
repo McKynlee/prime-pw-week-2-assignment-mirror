6074012a997e26621ba639e594f1e6bebd63cd24
McKynlee Westman <mckynlee.westman@gmail.com>
2020-12-09 19:31:56 -0600

    Completed through #14

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -17,24 +17,31 @@ let luckyNumber=9;
 // Refer back to the videos if you need help with this one.
 console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous=true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food='rotisserie chicken and roasted potatoes';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets=1;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets=0;
 // 11 - Add two pets to your `pets` variable
-
+pets +=2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const petsAllowed=1;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if ( adventurous ) {
+  console.log("Adventures are great!");
+}
+else {
+  console.log("How about we stay home?");
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if (luckyNumber===2 && adventurous) {
+  console.log("Roll the dice!");
+}
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
